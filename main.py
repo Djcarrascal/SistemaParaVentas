69c176481b803804fe101afea477af734e317f90
David Carrascal <d8carrascal@gmail.com>
2026-02-28 21:47:21 -0500

    comence a crear el codigo incial

---
+++ main.py
@@ -0,0 +1,6 @@
+print("\nBienvenido al nuevo sistema para registrar ventas\n" )
+print("Por favor ingresa los datos del cliente que te mencionaremos: \n")
+
+input("Nombre y apellido: ")
+input("\n Dirección: ")
+input("\n Si tiene membrecia escriba las siglas (ej: VIP), de lo contrario escriba (no)")
