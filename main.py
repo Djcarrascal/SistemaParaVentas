67d9e8151b469ef66707449d4582768a3f9c27a3
David Carrascal <d8carrascal@gmail.com>
2026-03-01 19:14:30 -0500

    Agregue nuevos inputs al codigo

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ print("\nBienvenido al nuevo sistema para registrar ventas\n" )
 print("Por favor ingresa los datos del cliente que te mencionaremos: \n")
 
 input("Nombre y apellido: ")
-input("\n Dirección: ")
-input("\n Si tiene membrecia escriba las siglas (ej: VIP), de lo contrario escriba (no)")
+input("\nDirección: ")
+input("\nSi tiene membrecia escriba las siglas (ej: VIP), de lo contrario escriba (NO): ")
+input("\nPrecio unitario del producto: ")
+input("\nIngrese la cantidad de productos: ")
+
