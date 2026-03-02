e7d2405acdb7934416122c8079fee46e9abce60b
David Carrascal <d8carrascal@gmail.com>
2026-03-01 19:50:41 -0500

    He realizado ajustes en los inputs de precio y cantidad

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+#Aqui capturamos los datos del cliente y la venta realizada
+
 print("\nBienvenido al nuevo sistema para registrar ventas\n" )
 print("Por favor ingresa los datos del cliente que te mencionaremos: \n")
 
 input("Nombre y apellido: ")
 input("\nDirección: ")
 input("\nSi tiene membrecia escriba las siglas (ej: VIP), de lo contrario escriba (NO): ")
-input("\nPrecio unitario del producto: ")
-input("\nIngrese la cantidad de productos: ")
+while True:
+    try:
+        precio=float(input("\nPrecio unitario del producto: "))
+        cantidad=int(input("\nIngrese la cantidad de productos: "))
+        break
+    except ValueError:
+        print("\n Dato ingresado inválido, solo se pueden ingresar números")
 
+subt = precio * cantidad
+
+print(f"\nEl valor total es: {subt:.3f} \n")
