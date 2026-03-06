abd840ba36941c50d53bd5b8c79b163333d5b3e8
David Carrascal <d8carrascal@gmail.com>
2026-03-06 12:52:10 -0500

    Ya deje listas las funcionalidades del sistema

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 #Aqui capturamos los datos del cliente y la venta realizada
 
 print("\nBienvenido al nuevo sistema para registrar ventas\n" )
-print("Por favor ingresa los datos del cliente que te mencionaremos: \n")
+print("Por favor ingresa los datos de la venta que solicitaremos \n")
 
-input("Nombre y apellido: ")
-input("\nDirección: ")
-input("\nSi tiene membrecia escriba las siglas (ej: VIP), de lo contrario escriba (NO): ")
-while True:
-    try:
-        precio=float(input("\nPrecio unitario del producto: "))
-        cantidad=int(input("\nIngrese la cantidad de productos: "))
-        break
-    except ValueError:
-        print("\n Dato ingresado inválido, solo se pueden ingresar números")
+nombre = input("Nombre y apellido: ")
+print("\nTiene el cliente membresia VIP? ")
+print("\n1. Si")
+print("2. No")
 
+vip = int(input("\nIngrese el número de su elección: "))
+while vip < 1 or vip > 2:
+    print("Opcion no valida")
+    vip = int(input("\nIngrese el número de su elección: "))
+
+precio=int(input("\nIngrese el precio unitario del producto: "))
+cantidad=int(input("\nIngrese la cantidad de productos: "))
+descuento = precio * cantidad * 0.10
 subt = precio * cantidad
 
-print(f"\nEl valor total es: {subt:.3f} \n")
+
+print("\n################################")
+
+print("\nFactura del cliente\n")
+
+print(f"Cliente: {nombre}")
+
+print(f"\nSubtotal: ${subt} ")
+if vip == 1:
+    print(f"Descuento: ${descuento}")
+else:
+    print("Descuento: $0")
+
+if vip == 1:
+    print(f"Total: ${subt - descuento}")
+else:
+    print(f"Total: ${subt}")
+
+print("\nGracias por su compra, vuelva pronto!\n")
+
+print("\n################################\n")
+
+#Fin del programa
